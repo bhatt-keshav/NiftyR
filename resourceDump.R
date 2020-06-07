49fb16120b7c7f7d59757d3b14e3a52a00fd78a2
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2020-06-07 13:23:18 +0200

    Create resourceDump.R

---
+++ resourceDump.R
@@ -0,0 +1,3 @@
+## 1 to search for a string in a list of lists (not safe, but does the job apparently)
+listoflists[grep('searchterm', listoflists)] # output: vector of indices
+# can also pipe length to it 
