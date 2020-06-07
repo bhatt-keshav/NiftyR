569a83688be6dc300b51cda0d2f114d68d1aafbe
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2020-06-07 13:27:12 +0200

    Update resourceDump.R

--- resourceDump.R
+++ resourceDump.R
@@ -1,3 +1,5 @@
 ## 1 to search for a string in a list of lists (not safe, but does the job apparently)
 listoflists[grep('searchterm', listoflists)] # output: vector of indices
 # can also pipe length to it 
+## 2 ni operator
+'%ni%' <- Negate('%in%')
