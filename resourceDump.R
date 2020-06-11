ee148cf76a428e3788938ecc1928a750730e7146
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2020-06-11 15:28:40 +0200

    Remove element by value from list of lists

--- resourceDump.R
+++ resourceDump.R
@@ -3,3 +3,17 @@ listoflists[grep('searchterm', listoflists)] # output: vector of indices
 # can also pipe length to it 
 ## 2 ni operator
 '%ni%' <- Negate('%in%')
+## 3 Remove element by value from list of lists
+# unlist is optional
+# y is the value you want to remove
+listoflists <- lapply(listoflists, setdiff, y="value_to_remove") %>% unlist()
+# e.g.
+> ingredients
+[[1]]
+[1] "primi piatti" ""             ""             ""             ""            
+[6] ""             ""             ""             ""             ""            
+[11] ""             ""             ""             ""             ""            
+[16] ""             ""             ""             ""             ""            
+[21] ""             "gnocchi"  
+> ingredients <- lapply(ingredients, setdiff, y="") %>% unlist()
+> [1] "primi piatti" "gnocchi"  
