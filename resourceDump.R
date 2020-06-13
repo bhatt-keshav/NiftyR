b8933dcfdc1ba11abcdfda07c1c8a7ab9525b1d0
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2020-06-13 13:18:14 +0200

    Update resourceDump.R

--- resourceDump.R
+++ resourceDump.R
@@ -17,3 +17,26 @@ listoflists <- lapply(listoflists, setdiff, y="value_to_remove") %>% unlist()
 [21] ""             "gnocchi"  
 > ingredients <- lapply(ingredients, setdiff, y="") %>% unlist()
 > [1] "primi piatti" "gnocchi"  
+
+## Call this function with a list or vector to wrap the labels in a barplot (by making \n between the text)
+# wrap.it is prerequisite for wrap.labels, use wrap.labels
+wrap.it <- function(x, len)
+{ 
+  sapply(x, function(y) paste(strwrap(y, len), 
+                              collapse = "\n"), 
+         USE.NAMES = FALSE)
+}
+# 
+wrap.labels <- function(x, len)
+{
+  if (is.list(x))
+  {
+    lapply(x, wrap.it, len)
+  } else {
+    wrap.it(x, len)
+  }
+}
+
+# e.g.
+wr.lap <- wrap.labels(recipeCategoryDFIT$category, 18); wr.lap
+# "contenuto sponsorizzato"  becomes "contenuto\nsponsorizzato" 
