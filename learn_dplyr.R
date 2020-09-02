599fea7b46c625cb2e0451641d82c411551ff46d
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2020-09-02 18:41:06 +0200

    Create learn_dplyr.R

---
+++ learn_dplyr.R
@@ -0,0 +1,18 @@
+### This file explains dplyr data manipulation verbs
+head(mtcars)
+dim(mtcars)
+# This counts all rows
+mtcars %>% summarise(tot = n())
+# This counts observations per cyl
+mtcars %>% group_by(cyl) %>% summarise(n_cyl = n())
+# Check
+mtcars %>% filter(cyl == 6)
+# This counts number of gears per cyl class
+mtcars %>% group_by(cyl, gear) %>% summarise(n_gears_per_cyl = n())
+# Check
+mtcars %>% filter(cyl == 4 & gear == 3)
+# This counts unique sub counts
+mtcars %>% group_by(cyl) %>% summarise(n_gears = n_distinct(gear))
+# Check: For cyl == 4, there are 3 unique type of gears: 4, 3, 5
+mtcars %>% select(cyl, gear) %>% filter(cyl == 4)
+
