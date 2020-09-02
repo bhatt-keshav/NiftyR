c7b5c895c78a3a3d014c3653064341588f935558
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2020-09-02 19:02:53 +0200

    Update learn_dplyr.R

--- learn_dplyr.R
+++ learn_dplyr.R
@@ -15,4 +15,8 @@ mtcars %>% filter(cyl == 4 & gear == 3)
 mtcars %>% group_by(cyl) %>% summarise(n_gears = n_distinct(gear))
 # Check: For cyl == 4, there are 3 unique type of gears: 4, 3, 5
 mtcars %>% select(cyl, gear) %>% filter(cyl == 4)
+# Creates a column with the max gear per cyl
+mtcars %>% group_by(cyl) %>% summarise(max_gear = max(gear))
+# Check
+mtcars %>% filter(cyl == 4)
 
